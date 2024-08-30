761c8c75fce4068ae92a40a1e573b18c10cc445e
ppanchal912 <ppanchal912@gmail.com>
2024-08-30 22:43:13 +0530

    first commit

---
+++ backend/src/api/brokers/applications/applications.module.ts
@@ -0,0 +1,8 @@
+import { BrokerApplicationsListModule } from './list-applications/list-applications.module';
+// import { BrokerApplicationsListModule } from './list-applications/list-applications.module';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [BrokerApplicationsListModule],
+})
+export class BrokerApplicationsModule {}
